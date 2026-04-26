48d42c3c72c4769518a72c975daac989253492f1
Ayush Kumar <ayushkumarranjan123@gmail.com>
2026-04-27 03:24:56 +0530

    completed login page

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import LoginPage from './components/loginPageComponents/loginPage.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+     <Routes>
+      <Route path='/' element={<App/>}></Route>
+      <Route path='/login' element={<LoginPage/>}></Route>
+     </Routes> 
     </BrowserRouter>
   </StrictMode>,
 )
