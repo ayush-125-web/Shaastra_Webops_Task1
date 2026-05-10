649a0bec0142280508287eb29181c455252c9762
Ayush Kumar <ayushkumarranjan123@gmail.com>
2026-05-11 02:43:50 +0530

    completed the project

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
     <CartProvider>
       <Routes>
-        <Route path='/' element={<Route path='/' element={<App/>}/>}></Route>
+        <Route path='/' element={<App/>}></Route>
         <Route path='/login' element={<LoginPage/>}></Route>
         <Route path='/explore' element={<Explore/>}/>
         <Route path='/cart' element={<Cart/>}></Route>
